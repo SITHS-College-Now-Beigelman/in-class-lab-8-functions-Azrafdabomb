7c2bcf1ab3aaedf39d812475cd241ff286d1c466
FloppyDuck <158211952+Azrafdabomb@users.noreply.github.com>
2024-11-04 16:27:24 -0500

    hello

---
+++ Hossain_Azraf_Lab8.cpp
@@ -0,0 +1,25 @@
+// Azraf Hossain
+// 11/4/24
+// Lab 8
+
+#include <iostream>  // Include for input and output operations
+#include <cmath>      // Include for mathematical functions
+#include <cctype>     // Include for character handling functions
+#include <iomanip>    // Include for manipulators used to format output
+
+using namespace std; // Makes all the names in the std namespace available
+
+
+int doubleMultiply(int a, int b)
+{
+    int product;
+    product - a * b * 2;
+    return product;
+}
+int main() 
+{
+    int num 1, num 2;
+    cout << "Enter two intge"
+
+    return 0;
+}
